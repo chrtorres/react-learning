7c8481c4e9e2560fe5fb637013fbe23cec1e24fc
Christopher <chris.rtorres@outlook.com>
2023-11-02 14:17:05 -0400

    Finished password Generator with React + Vite

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { 
+  useState, 
+  useCallback, 
+  useEffect, 
+  useRef 
+} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   // need a length, characters allowed (letters, numbers, etc.)
   // need state of password
-  const [length, setLength] = useState(0);
+  const [length, setLength] = useState(6);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false); 
   const [password, setPassword] = useState('');
+
+  const passwordRef = useRef(null);
+
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = 
+    "ABCDEFGHIJKLMNOPKRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    if (numAllowed)
+      str += "0123456789";
+    if (charAllowed)
+      str += "!@#$%^&*()_+";
+
+      for (let i = 1; i < length; i++) {
+        const char = Math.floor(Math.random() * str.length + 1);
+        pass += str.charAt(char);
+      }
+      setPassword(pass);
+
+  }, [length, numAllowed, charAllowed]);
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select();
+  };
+
+
+  useEffect(() => {     // this allows for the password to be re-rendered every time 
+    generatePassword(); // user changes length or checkboxes, useEffect() is popular
+  }, [length, numAllowed, charAllowed, generatePassword]);
 
   return (
     <>
@@ -25,11 +61,54 @@ function App() {
           className='outline-non w-full py-1 px-3'
           placeholder='Password'
           readOnly
+          ref={passwordRef}   // keeps reference to password in order track actions
           />
           <button
+          onClick={copyPasswordToClipboard} // without () to use, otherwise it is called
           className='out bg-blue-700 text-white px-3
           py-0.5 shrink-0'
           >copy</button>
+        </div>
+        <div
+        className='flex text-sm gap-x-2'
+        >
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="range"
+            min={6}
+            max={25}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => setLength(e.target.value)} // strip the value from the event e
+            name=''
+            id='length' 
+            />
+            <label htmlFor='length'>Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="checkbox"
+            defaultChecked={numAllowed}   // default state
+            onChange={() => {
+              setNumAllowed((prev) => !prev)  // captures current value as 'prev' in a 
+            }}                                // callback to respond in real time
+            name=''
+            id='number' 
+            />
+            <label htmlFor='number'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="checkbox"
+            defaultChecked={charAllowed}
+            onChange={() => {
+              setcharAllowed((prev) => !prev)
+            }} 
+            name=''
+            id='charInput' 
+            />
+            <label htmlFor='charInput'>Characters</label>
+          </div>
         </div>
       </div>
     </>
